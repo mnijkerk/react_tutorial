00816b78163d8a37235fc733fec0185969ce9de4
mnijkerk <maximiliannijkerk2021@u.northwestern.edu>
2022-10-09 22:18:00 -0500

    filter buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -10,37 +10,7 @@ import './CourseList.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch';
-import CourseListIndiv from './components/CourseListIndiv';
 
-// const schedule = {
-//   "title": "CS Courses for 2018-2019",
-//   "courses": {
-//     "F101" : {
-//       "term": "Fall",
-//       "number": "101",
-//       "meets" : "MWF 11:00-11:50",
-//       "title" : "Computer Science: Concepts, Philosophy, and Connections"
-//     },
-//     "F110" : {
-//       "term": "Fall",
-//       "number": "110",
-//       "meets" : "MWF 10:00-10:50",
-//       "title" : "Intro Programming for non-majors"
-//     },
-//     "S313" : {
-//       "term": "Spring",
-//       "number": "313",
-//       "meets" : "TuTh 15:30-16:50",
-//       "title" : "Tangible Interaction Design and Learning"
-//     },
-//     "S314" : {
-//       "term": "Spring",
-//       "number": "314",
-//       "meets" : "TuTh 9:30-10:50",
-//       "title" : "Tech & Human Interaction"
-//     }
-//   }
-// };
 
 const Main = () => {
   const [schedule, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
@@ -61,6 +31,8 @@ const Main = () => {
       <body className='App-body'>
         
         <CourseList schedule= {schedule} /> 
+
+        {/* <Termfilter schedule={schedule}/> */}
 
       </body>
 
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,28 +1,79 @@
-import CourseListIndiv from "./CourseListIndiv";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../CourseList.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../CourseList.css";
 
+import { useState } from "react";
 
-const CourseList = ({schedule}) => (
-    
-    <div className='courseList'>
-        
-        { Object.entries(schedule.courses).map(([id, course]) => 
-        
-        // hello
+// const terms = {
+//   Fall: "Breakfast items...",
+//   Winter: "Lunch items...",
+//   Spring: "Dinner items...",
+// };
+
+const terms = ['Fall', 'Winter', 'Spring'];
+
+const FilterButton = ({ term, selection, setSelection }) => (
+  <button className="filterButtons" >
+    <input
+      type="radio"
+      id={term}
+      className="btn-check"
+      checked={term === selection}
+      autoComplete="off"
+      onChange={() => setSelection(term)}
+    />
+    <label htmlFor={term}>
+      {term}
+      
+    </label>
+  </button>
+);
+
+const FilterSelector = ({ selection, setSelection }) => (
+  <div className="btn-group">
+    {terms.map((term) => (
+      <FilterButton
+        key={term}
+        term={term}
+        selection={selection}
+        setSelection={setSelection}
+      />
+    ))}
+  </div>
+);
+
+const FilteredTiles = ({ schedule, termSelection }) => {
+  const filteredSchedule = Object.entries(schedule.courses).filter(
+    ([id, course]) => course.term == termSelection
+  );
+
+  return (
+    <div className="courseList">
+      {filteredSchedule.map(([id, course]) => (
         <div className="courseTile">
+          <div className="courseNumberDescription">
+            <h3>
+              {course.term} CS {course.number}{" "}
+            </h3>
+            <p> {course.title} </p>
+          </div>
 
-            <div className="courseNumberDescription">
-                <h3>{course.term} CS {course.number} </h3>
-                <p> {course.title} </p>
-            </div>
-            
-            <div className="meetingTime"> 
-                {course.meets}
-            </div>
+          <div className="meetingTime">{course.meets}</div>
         </div>
-        )} 
+      ))}
     </div>
-)
-  export default CourseList; 
+  );
+};
+
+const CourseList = ({ schedule }) => {
+    const [selection, setSelection] = useState(() => terms[0]);
+    return (
+      <div>
+        <FilterSelector selection={selection} setSelection={setSelection} />
+        <FilteredTiles schedule={schedule} termSelection={selection} />
+      </div>
+    );
+  };
+  
+  export default CourseList;
+  
