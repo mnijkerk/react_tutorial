90ee3d9fa5f1137cddcff057760f76a8adc90af8
mnijkerk <maximiliannijkerk2021@u.northwestern.edu>
2022-11-09 18:29:11 -0600

    fixed bug

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -7,15 +7,15 @@ describe ('Test App', () => {
     cy.visit ('/');
   });
 
-  it ('opens with Fall CS courses', () => {
-    cy.visit ('/');
-    cy.get('[data-cy=course]').should('contain', 'Fall CS');
-  });
-
+  
   it ('Click winter and get courses Winter CS courses', () => {
     cy.visit ('/');
     cy.get('[data-cy=Winter]').click()
     cy.get('[data-cy=course]').should('contain', 'Winter CS');
+  });
+  it ('opens with Fall CS courses', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=course]').should('contain', 'Fall CS');
   });
 
 
